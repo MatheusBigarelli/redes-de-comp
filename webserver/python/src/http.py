2d96cbd8ac3f7d4118656cd285853624165a405e
Matheus <bigarellimatheus@gmail.com>
2019-08-30 09:00:50 -0300

    Algumas funcionalidades feitas

--- webserver/python/src/http.py
+++ webserver/python/src/http.py
@@ -1,3 +1,7 @@
+import time
+import os
+
+
 class HTTPRequest:
     CRLF = "\r\n"
     VERSION = "HTTP/1.0 "
@@ -5,9 +9,60 @@ class HTTPRequest:
     NOT_FOUND = "404 Not found\r\n"
     MOVED = "304 Moved permanently\r\n"
 
+
     def __init__(self, request):
         self.request = request
 
-    def parse(self, kwrdargs):
-        lines = self.request.split(CRLF)
+    def parse(self, **kwargs):
+        lines = self.request.split(HTTPRequest.CRLF)
+        self.requested_file = lines[0].split(' ')[1]
+        if self.requested_file == '/':
+            self.requested_file = 'index'
+        if not '.html' in self.requested_file:
+            self.requested_file += '.html'
+
+        for line in lines[1:]:
+            if 'Connection' in line:
+                self.conn_type = line
         
+        self.date = time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.gmtime()) + '\r\n'
+
+        
+        self.path = 'www/' + self.requested_file
+    
+    def buildResponse(self):
+        string = ''
+        string = self.appendHeader(string)
+        string = self.appendPayload(string)
+        
+        return string.encode()
+
+
+    def appendHeader(self, string):
+
+        with open(self.path, 'r') as html_file:
+            l = len(html_file.read())
+            html_file.close()
+
+        string += "HTTP/1.0 200 OK\r\n"
+        string += "Connection: close\r\n"
+        string += "Server: Seu cu\r\n"
+        string += 'Content-Length: ' + str(l) + '\r\n'
+        string += 'Content-Type: text/html; charset=utf-8\r\n'
+        string += 'Date: ' + self.date
+        string += '\r\n'
+
+        return string
+
+
+    def appendPayload(self, string):
+        if (os.path.exists(self.path)):
+            with open(self.path, 'r') as html_file:
+                for line in html_file.readlines():
+                    string += line
+                html_file.close()
+
+        else:
+            string += "404 Not Found"
+
+        return string
--- webserver/python/src/server.py
+++ webserver/python/src/server.py
@@ -1,17 +1,49 @@
+from .http import HTTPRequest
 import socket
-from threading import Thread
 
-class Server():
+class WebServer():
+    # Index of connection in client tuple
+    CONNECTION = 0
+    # Index of client address in client tuple
+    ADDRESS = 1
 
-    def __init__(self, value):
+    def __init__(self, port):
         super().__init__()
-        self.value = value
+        self.host = ''
+        self.port = port
+        self.clients = []
 
-    def run(self):
-        print(self.value)
 
+    def handle_connection(self, client):
+        request = client[WebServer.CONNECTION].recv(1024).decode()
+        http_request = HTTPRequest(request)
+        http_request.parse()
+        response = http_request.buildResponse()
+        client[WebServer.CONNECTION].send(response)
+        client[WebServer.CONNECTION].close()
+    
+    def start(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((self.host, self.port))
+            self.socket.listen(5)
 
-if __name__ == "__main__":
-    serv = Server()
+            connection, address = self.socket.accept()
+            client = (connection, address)
+            self.clients.append(client)
+            self.handle_connection(client)
+            
 
-    serv.start()
+        except KeyboardInterrupt:
+            print('Server stopping...')
+            if (len(self.clients)):
+                for client in self.clients:
+                    client[WebServer.CONNECTION].close()
+            if (self.socket):
+                self.socket.close()
+
+# if __name__ == "__main__":
+#     port = 4444
+#     serv = WebServer(port)
+
+#     serv.start()
